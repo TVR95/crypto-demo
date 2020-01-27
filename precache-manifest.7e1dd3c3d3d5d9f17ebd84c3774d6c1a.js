422ac1fc2e3b2ba6f1a36196cf86b518402b88b1
TVR95 <kamil.szczepanski1@gmail.com>
2020-01-27 13:44:36 +0100

    Updates

---
+++ precache-manifest.7e1dd3c3d3d5d9f17ebd84c3774d6c1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc45ea7645ac2f10b5d11fdb13e8de63",
+    "url": "/crypto-demo/index.html"
+  },
+  {
+    "revision": "06bc15c5be80a27c9362",
+    "url": "/crypto-demo/static/css/main.165d4ccf.chunk.css"
+  },
+  {
+    "revision": "a4eeb4c316089b01b302",
+    "url": "/crypto-demo/static/js/2.de439a99.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/crypto-demo/static/js/2.de439a99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06bc15c5be80a27c9362",
+    "url": "/crypto-demo/static/js/main.bf8e3de5.chunk.js"
+  },
+  {
+    "revision": "a7e37a320480135f13d8",
+    "url": "/crypto-demo/static/js/runtime-main.6ea3478a.js"
+  },
+  {
+    "revision": "2b0168d1d7bdaad1194c143539ae87df",
+    "url": "/crypto-demo/static/media/stanislaw-zarychta-5FWx5YNMxok-unsplash.2b0168d1.jpg"
+  }
+]);
